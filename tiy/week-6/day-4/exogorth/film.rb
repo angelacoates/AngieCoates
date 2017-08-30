85b78be0d6a6e2c73bea1ae6f26bc4ef8cbe1cf7
Angela Coates <angela@angelamarii.com>
2017-08-29 20:13:38 -0500

    Initial commit

--- tiy/week-6/day-4/exogorth/film.rb
+++ tiy/week-6/day-4/exogorth/film.rb
@@ -3,11 +3,10 @@ require 'json'
 require 'ap'
 
 class Film
-    def get_opening_crawl(episode_identifier)
-      response = HTTParty.get("http://swapi.co/api/films")
-      @questions = JSON.parse(response.body)
-      @answer = @questions["results"].find { |result|
-      result["episode_id"] = episode_identifier }
+  def get_opening_crawl(episode_identifier)
+    response = HTTParty.get("http://swapi.co/api/films")
+    @questions = JSON.parse(response.body)
+    @answer = @questions["results"].find {|result|result["episode_id"] = episode_identifier}
     return @answer["opening_crawl"]
   end
 end
